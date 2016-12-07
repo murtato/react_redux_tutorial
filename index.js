5ec0e8e29e41df2df7168cbac384cb8defb91532
Victor Tran <murtato@gmail.com>
2016-12-06 16:40:26 -0800

    deleted previous files from vijayxtreme react redux tutorial

--- index.js
+++
@@ -1,8 +0,0 @@
-// React Redux exercise
-// see github profile for vijayxtreme for the full app.
-// npm install react
-
-// learn about .props to learn about children and parents inheritance method in React
-
-// wth is MobX?
-// a simpler way to manage MVC
