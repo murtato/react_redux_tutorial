9cc665fdac8792771447725d871ee42ebb224ca9
Victor Tran <murtato@gmail.com>
2016-12-14 19:19:22 -0800

    configured components to render on page

--- src/index.js
+++ src/index.js
@@ -1,22 +1,42 @@
-// packages installed through npm do not need folder extensions
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
-// denote folder extensions if importing files made by self
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyDsnMggqtdMnqnUbwWb27GQuOAIk0RZTFk';
 
-// Create a new component. This component
-// should produce some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+// Create new component. Component should produce HTML
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+          videos={this.state.videos} />
+      </div>
+    );
+  };
 }
 
-// Take this component's generated HTML and
-// put it on the page (in the DOM)
+
 ReactDOM.render(<App />, document.querySelector('.container'));
